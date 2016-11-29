ed36cd8634429e81a9aacf1282c02d0ef1d90161
ErnestoPM <ernestopm20@gmail.com>
2016-11-28 23:39:09 -0600

    Cambiando about us

--- public/js/controllers/aboutCtrl.js
+++ public/js/controllers/aboutCtrl.js
@@ -1,8 +1,31 @@
 angular.module('sampleApp').controller('aboutCtrl', aboutCtrl);
-aboutCtrl.$inject = ['d3service','$rootScope','$routeParams','$mdDialog', '$mdSidenav','$interval','$mdToast','$scope','$timeout','Upload'];
+aboutCtrl.$inject = ['d3service','$rootScope','$routeParams','$mdDialog', '$mdSidenav','$interval','$mdToast','$scope','$timeout','Upload','$scope'];
 
-function aboutCtrl(d3service,$rootScope,$routeParams,$mdDialog,$mdSidenav,$interval,$mdToast,$scope,$timeout,Upload) {
+function aboutCtrl(d3service,$rootScope,$routeParams,$mdDialog,$mdSidenav,$interval,$mdToast,$scope,$timeout,Upload,$scope) {
     var vm = this;
 
+
+
+    $scope.$on('$viewContentLoaded', function(){
+
+        var example = Popcorn.vimeo('#video', 'http://player.vimeo.com/video/139416145' );
+
+        example.footnote({
+            start:2,
+            end: 6,
+            text: "Pop!",
+            target: 'footnotediv'
+        });
+
+        example.footnote({
+            start: 8,
+            end: 18,
+            text: "Pop 2 de nuevo jaja",
+            target: 'footnotediv'
+        });
+
+
+        console.log(example);
+    });
 
 }
